0d3ac4fc0379d79329d6b5fce5fb95cb63950e6e
eemilyy <emilyalmeeidas@gmail.com>
2023-01-24 20:14:26 +0000

    atualizando userdata

--- script/userdata.sh
+++ script/userdata.sh
@@ -4,5 +4,6 @@ yum install -y httpd
 systemctl start httpd
 systemctl enable httpd
 yum install -y git
+cd /
 mkdir /nfs
 mount -t nfs -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport fs-0f1683df7dc391f1b.efs.us-east-1.amazonaws.com:/ /nfs
