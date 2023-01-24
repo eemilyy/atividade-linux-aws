bcd5b461ca438c9726c24a0fba1db8b31ba4e13b
eemilyy <emilyalmeeidas@gmail.com>
2023-01-24 17:26:43 +0000

    alterando script para gerar saida correta

--- script/logservicehttp.sh
+++ script/logservicehttp.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-data=$(date +%d/%m/%Y-%H:%M:%S)
+data=$(date +%d/%h/%Y-%H:%M:%S)
 if systemctl --type=service --state=running | grep httpd.service
 then
-        echo "$data - httpd.service - online - Apache_ligado" | tee -a /script/log/logOnhttp
+        echo "[ $data ] - httpd - online - Servico Apache ligado" | tee -a /nfs/emily/log/apacheOn
 
 else
-        echo "$data - httpd.service - offline - Apache_desligado" | tee -a /script/log/logOffhttp
+        echo "[ $data ] - httpd - offline - Servico Apache desligado" | tee -a /nfs/emily/log/apacheOff
 fi
